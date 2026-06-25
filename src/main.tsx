3885546bef65e3eb1341277bdcd027c38868f870
Ricardo Ortega <rikrdo.ortega@gmail.com>
2026-06-25 15:31:32 -0600

    chore: initial Phaser 3 + Vite + TS scaffold

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import Phaser from "phaser";
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: "#0a0a0f",
+  scene: [],
+};
+
+new Phaser.Game(config);
