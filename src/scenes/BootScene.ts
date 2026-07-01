4d7d45d0124a728391328e12e1997d4a8cc6ed1c
Ricardo Ortega <rikrdo.ortega@gmail.com>
2026-06-30 22:41:44 -0600

    chore: add prettier config and eslint-config-prettier

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -7,16 +7,43 @@ export class BootScene extends Phaser.Scene {
 
   preload() {
     // Enemy3 sprites
-    this.load.spritesheet("enemy3_idle", "/assets/enemies/enemy3_idle.png", { frameWidth: 64, frameHeight: 64 });
-    this.load.spritesheet("enemy3_fly", "/assets/enemies/enemy3_fly.png", { frameWidth: 64, frameHeight: 64 });
-    this.load.spritesheet("enemy3_hit", "/assets/enemies/enemy3_hit.png", { frameWidth: 64, frameHeight: 64 });
-    this.load.spritesheet("enemy3_die", "/assets/enemies/enemy3_die.png", { frameWidth: 64, frameHeight: 64 });
-    this.load.spritesheet("enemy3_attack_start", "/assets/enemies/enemy3_attack_smash_start.png", { frameWidth: 64, frameHeight: 64 });
-    this.load.spritesheet("enemy3_attack_loop", "/assets/enemies/enemy3_attack_smash_loop.png", { frameWidth: 64, frameHeight: 64 });
-    this.load.spritesheet("enemy3_smash_end", "/assets/enemies/enemy3_smash_end.png", { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet("enemy3_idle", "/assets/enemies/enemy3_idle.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.spritesheet("enemy3_fly", "/assets/enemies/enemy3_fly.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.spritesheet("enemy3_hit", "/assets/enemies/enemy3_hit.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.spritesheet("enemy3_die", "/assets/enemies/enemy3_die.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+    this.load.spritesheet(
+      "enemy3_attack_start",
+      "/assets/enemies/enemy3_attack_smash_start.png",
+      { frameWidth: 64, frameHeight: 64 },
+    );
+    this.load.spritesheet(
+      "enemy3_attack_loop",
+      "/assets/enemies/enemy3_attack_smash_loop.png",
+      { frameWidth: 64, frameHeight: 64 },
+    );
+    this.load.spritesheet(
+      "enemy3_smash_end",
+      "/assets/enemies/enemy3_smash_end.png",
+      { frameWidth: 64, frameHeight: 64 },
+    );
 
     // Moth
-    this.load.spritesheet("moth", "/assets/enemies/fly_spook_moth.png", { frameWidth: 25, frameHeight: 25 });
+    this.load.spritesheet("moth", "/assets/enemies/fly_spook_moth.png", {
+      frameWidth: 25,
+      frameHeight: 25,
+    });
 
     // Background layers
     this.load.image("bg_far", "/assets/background/far_buildings.png");
@@ -24,14 +51,17 @@ export class BootScene extends Phaser.Scene {
     this.load.image("bg_foreground", "/assets/background/2_foreground.png");
 
     // Duck companion
-    this.load.spritesheet("duck", "/assets/duck/duck_sprite.png", { frameWidth: 88, frameHeight: 66 });
+    this.load.spritesheet("duck", "/assets/duck/duck_sprite.png", {
+      frameWidth: 88,
+      frameHeight: 66,
+    });
 
     // Music
     this.load.audio("bgm", "/assets/music/cyberpunk_street.ogg");
   }
 
   create() {
-  console.log("BootScene ready");
-  // Will transition to GameScene once assets are loaded
+    console.log("BootScene ready");
+    // Will transition to GameScene once assets are loaded
   }
 }
