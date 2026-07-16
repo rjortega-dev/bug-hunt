3c1c776c1664475a59e4cd632975883746d34781
Ricardo Ortega <rikrdo.ortega@gmail.com>
2026-07-16 14:07:54 -0600

    refactor: modified the pause resume logic to always pause the game when user leaves game, also forcing the music to pause

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -13,6 +13,7 @@ export class GameScene extends Phaser.Scene {
   private readonly enemyCount = 3;
   private score: number = 0;
   private isManuallyPaused: boolean = false;
+  private bgm!: Phaser.Sound.BaseSound;
 
   create() {
     const bg_back = this.add.image(400, 300, "bg_back").setScrollFactor(0);
@@ -37,12 +38,11 @@ export class GameScene extends Phaser.Scene {
     });
 
     this.game.events.on("blur", () => {
-      this.scene.pause();
-    });
-
-    this.game.events.on("focus", () => {
       if (!this.isManuallyPaused) {
-        this.scene.resume();
+        this.isManuallyPaused = true;
+        this.scene.pause();
+        this.bgm?.pause();
+        controls.setPauseLabel("Resume");
       }
     });
   }
@@ -51,9 +51,11 @@ export class GameScene extends Phaser.Scene {
     this.isManuallyPaused = !this.isManuallyPaused;
     if (this.isManuallyPaused) {
       this.scene.pause();
+      this.bgm?.pause();
       controls.setPauseLabel("Resume");
     } else {
       this.scene.resume();
+      this.bgm?.resume();
       controls.setPauseLabel("Pause");
     }
   }
@@ -70,7 +72,8 @@ export class GameScene extends Phaser.Scene {
       this.enemies.push(enemy);
     }
 
-    this.sound.play("bgm", { loop: true, volume: 0.5 });
+    this.bgm = this.sound.add("bgm", { loop: true, volume: 0.5 });
+    this.bgm.play();
   }
 
   update() {
